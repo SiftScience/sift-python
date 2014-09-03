5e66f5aaefa2d84920fa484bf8a83b2ff0ca3847
Yoav Schatzberg <yoav@siftscience.com>
2014-09-03 11:59:39 -0700

    Bumping version to 1.1.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 setup(
     name='Sift',
     description='Python bindings for Sift Science\'s API',
-    version='1.1.0.0', # NB: must be kept in sync with sift/version.py
+    version='1.1.1.0', # NB: must be kept in sync with sift/version.py
     url='https://siftscience.com',
 
     author='Sift Science',
