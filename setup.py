ced8c43b1650c98c2ccf35b78f3b760fd2e096d6
Fred Sadaghiani <fred.sadaghiani@gmail.com>
2016-02-08 16:01:18 -0800

    Fix code formatting to conform to pep8 standard

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except:
 setup(
     name='Sift',
     description='Python bindings for Sift Science\'s API',
-    version='1.1.2.3', # NB: must be kept in sync with sift/version.py
+    version='1.1.2.4',  # NB: must be kept in sync with sift/version.py
     url='https://siftscience.com',
 
     author='Sift Science',
@@ -32,7 +32,7 @@ setup(
         "mock >= 1.0.1",
     ],
 
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
--- sift/client.py
+++ sift/client.py
@@ -8,9 +8,9 @@ import traceback
 import warnings
 import sys
 if sys.version_info[0] < 3:
-  import urllib
+    import urllib
 else:
-  import urllib.parse as urllib
+    import urllib.parse as urllib
 
 import sift
 from . import version
@@ -19,7 +19,8 @@ API_URL = 'https://api.siftscience.com'
 
 
 class Client(object):
-    def __init__(self, api_key = None, api_url=API_URL, timeout=2.0):
+
+    def __init__(self, api_key=None, api_url=API_URL, timeout=2.0):
         """Initialize the client.
 
         Args:
@@ -34,7 +35,7 @@ class Client(object):
             raise RuntimeError("api_url must be a string")
 
         if api_key is None:
-          api_key = sift.api_key
+            api_key = sift.api_key
 
         if not isinstance(api_key, str) or len(api_key.strip()) == 0:
             raise RuntimeError("valid api_key is required")
@@ -43,12 +44,13 @@ class Client(object):
         self.url = api_url + '/v%s' % version.API_VERSION
         self.timeout = timeout
         if sys.version_info[0] < 3:
-          self.UNICODE_STRING = basestring
+            self.UNICODE_STRING = basestring
         else:
-          self.UNICODE_STRING = str
+            self.UNICODE_STRING = str
 
     def user_agent(self):
-        return 'SiftScience/v%s sift-python/%s' % (version.API_VERSION, version.VERSION)
+        return 'SiftScience/v%s sift-python/%s' % (
+            version.API_VERSION, version.VERSION)
 
     def event_url(self):
         return self.url + '/events'
@@ -59,7 +61,13 @@ class Client(object):
     def label_url(self, user_id):
         return self.url + '/users/%s/labels' % urllib.quote(user_id)
 
-    def track(self, event, properties, path=None, return_score=False, timeout = None):
+    def track(
+            self,
+            event,
+            properties,
+            path=None,
+            return_score=False,
+            timeout=None):
         """Track an event and associated properties to the Sift Science client.
         This call is blocking.  Check out https://siftscience.com/resources/references/events_api
         for more information on what types of events you can send and fields you can add to the
@@ -70,7 +78,7 @@ class Client(object):
                 event name such as "$transaction" or "$create_order" or a custom event
                 name (that does not start with a $).
             properties: A dict of additional event-specific attributes to track
-            return_score: Whether the API response should include a score for this 
+            return_score: Whether the API response should include a score for this
                  user (the score will be calculated using this event).  This feature must be
                  enabled for your account in order to use it.  Please contact
                  support@siftscience.com if you are interested in using this feature.
@@ -79,36 +87,42 @@ class Client(object):
             a subclass of requests.exceptions.RequestException indicating the
             exception that occurred.
         """
-        if not isinstance(event, self.UNICODE_STRING) or len(event.strip()) == 0:
+        if not isinstance(
+                event, self.UNICODE_STRING) or len(
+                event.strip()) == 0:
             raise RuntimeError("event must be a string")
 
         if not isinstance(properties, dict) or len(properties) == 0:
             raise RuntimeError("properties dictionary may not be empty")
 
-        headers = { 'Content-type' : 'application/json',
-                    'Accept' : '*/*',
-                    'User-Agent' : self.user_agent() }
+        headers = {'Content-type': 'application/json',
+                   'Accept': '*/*',
+                   'User-Agent': self.user_agent()}
 
         if path is None:
-          path = self.event_url()
+            path = self.event_url()
 
         if timeout is None:
             timeout = self.timeout
 
-        properties.update({ '$api_key': self.api_key, '$type': event })
+        properties.update({'$api_key': self.api_key, '$type': event})
         params = {}
         if return_score:
-          params.update({ 'return_score' : return_score })
+            params.update({'return_score': return_score})
         try:
-            response = requests.post(path, data=json.dumps(properties),
-                    headers=headers, timeout=timeout, params=params)
+            response = requests.post(
+                path,
+                data=json.dumps(properties),
+                headers=headers,
+                timeout=timeout,
+                params=params)
             return Response(response)
         except requests.exceptions.RequestException as e:
             warnings.warn('Failed to track event: %s' % properties)
             warnings.warn(traceback.format_exc())
             return e
 
-    def score(self, user_id, timeout = None):
+    def score(self, user_id, timeout=None):
         """Retrieves a user's fraud score from the Sift Science API.
         This call is blocking.  Check out https://siftscience.com/resources/references/score_api.html
         for more information on our Score response structure
@@ -121,25 +135,30 @@ class Client(object):
             a subclass of requests.exceptions.RequestException indicating the
             exception that occurred.
         """
-        if not isinstance(user_id, self.UNICODE_STRING) or len(user_id.strip()) == 0:
+        if not isinstance(
+                user_id, self.UNICODE_STRING) or len(
+                user_id.strip()) == 0:
             raise RuntimeError("user_id must be a string")
 
         if timeout is None:
             timeout = self.timeout
 
-        headers = { 'User-Agent' : self.user_agent() }
-        params = { 'api_key': self.api_key }
+        headers = {'User-Agent': self.user_agent()}
+        params = {'api_key': self.api_key}
 
         try:
-            response = requests.get(self.score_url(user_id),
-                    headers=headers, timeout=timeout, params=params)
+            response = requests.get(
+                self.score_url(user_id),
+                headers=headers,
+                timeout=timeout,
+                params=params)
             return Response(response)
         except requests.exceptions.RequestException as e:
             warnings.warn('Failed to get score for user %s' % user_id)
             warnings.warn(traceback.format_exc())
             return e
 
-    def label(self, user_id, properties, timeout = None):
+    def label(self, user_id, properties, timeout=None):
         """Labels a user as either good or bad through the Sift Science API.
         This call is blocking.  Check out https://siftscience.com/resources/references/labels_api.html
         for more information on what fields to send in properties.
@@ -154,12 +173,18 @@ class Client(object):
             a subclass of requests.exceptions.RequestException indicating the
             exception that occurred.
         """
-        if not isinstance(user_id, self.UNICODE_STRING) or len(user_id.strip()) == 0:
+        if not isinstance(
+                user_id, self.UNICODE_STRING) or len(
+                user_id.strip()) == 0:
             raise RuntimeError("user_id must be a string")
 
-        return self.track('$label', properties, self.label_url(user_id), timeout=timeout)
+        return self.track(
+            '$label',
+            properties,
+            self.label_url(user_id),
+            timeout=timeout)
 
-    def unlabel(self, user_id, timeout = None):
+    def unlabel(self, user_id, timeout=None):
         """unlabels a user through the Sift Science API.
         This call is blocking.  Check out https://siftscience.com/resources/references/labels_api.html
         for more information.
@@ -173,26 +198,30 @@ class Client(object):
             a subclass of requests.exceptions.RequestException indicating the
             exception that occurred.
         """
-        if not isinstance(user_id, self.UNICODE_STRING) or len(user_id.strip()) == 0:
+        if not isinstance(
+                user_id, self.UNICODE_STRING) or len(
+                user_id.strip()) == 0:
             raise RuntimeError("user_id must be a string")
 
         if timeout is None:
             timeout = self.timeout
 
-        headers = { 'User-Agent' : self.user_agent() }
-        params = { 'api_key': self.api_key }
+        headers = {'User-Agent': self.user_agent()}
+        params = {'api_key': self.api_key}
 
         try:
 
-            response = requests.delete(self.label_url(user_id),
-                    headers=headers, timeout=timeout, params=params)
+            response = requests.delete(
+                self.label_url(user_id),
+                headers=headers,
+                timeout=timeout,
+                params=params)
             return Response(response)
 
         except requests.exceptions.RequestException as e:
             warnings.warn('Failed to unlabel user %s' % user_id)
             warnings.warn(traceback.format_exc())
             return e
-
 
 
 class Response(object):
@@ -210,7 +239,7 @@ class Response(object):
         self.url = http_response.url
 
         if (self.http_status_code not in self.HTTP_CODES_WITHOUT_BODY) \
-            and 'content-length' in http_response.headers:
+                and 'content-length' in http_response.headers:
             self.body = http_response.json()
             self.api_status = self.body['status']
             self.api_error_message = self.body['error_message']
@@ -222,7 +251,7 @@ class Response(object):
 
     def __str__(self):
         return ('{%s "http_status_code": %s}' %
-                ('' if self.body == None else '"body": ' +
+                ('' if self.body is None else '"body": ' +
                  json.dumps(self.body) + ',', str(self.http_status_code)))
 
     def is_ok(self):
@@ -231,5 +260,3 @@ class Response(object):
             return 204 == self.http_status_code
 
         return self.api_status == 0
-
-
