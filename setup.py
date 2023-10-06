7015e78979c839334cf525f3737e197272d0750e
Cameron Graybill <camerongraybill@gmail.com>
2023-10-06 16:47:06 -0400

    cleaning up implementation a bit to use `imp` unless unavailable

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
-import importlib.util
-import importlib.machinery
+try:
+    from imp import load_source
+except ImportError:
+    import importlib.util
+    import importlib.machinery
+
+    def load_source(modname, filename):
+        loader = importlib.machinery.SourceFileLoader(modname, filename)
+        spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+        module = importlib.util.module_from_spec(spec)
+        # The module is always executed and not cached in sys.modules.
+        # Uncomment the following line to cache the module.
+        # sys.modules[module.__name__] = module
+        loader.exec_module(module)
+        return module
+
+
 import os
 
 try:
@@ -17,16 +32,6 @@ except Exception:
     README = ''
     CHANGES = ''
 
-
-def load_source(modname, filename):
-    loader = importlib.machinery.SourceFileLoader(modname, filename)
-    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
-    module = importlib.util.module_from_spec(spec)
-    # The module is always executed and not cached in sys.modules.
-    # Uncomment the following line to cache the module.
-    # sys.modules[module.__name__] = module
-    loader.exec_module(module)
-    return module
 
 
 # Use imp to avoid sift/__init__.py
